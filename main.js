dc256efd630d74d57d93ebeede9a5878f3ed3da9
Rodrigo Figueredo <59716658+rgfigueredo91@users.noreply.github.com>
2020-04-23 21:38:11 -0300

    sa

--- main.js
+++ main.js
@@ -4,12 +4,21 @@ $(document).ready(function () {
   $('.submit').click(function (event){
     
     console.log('clicked button')
-
+    var name = $('.name').val()
     var email = $('.email').val()
     var subject = $('.subject').val()
     var message = $('.message').val()
     var statusElm = $('.status')
     statusElm.empty()
+
+
+
+     if(message.length > 1) {
+      statusElm.append('')
+    } else {
+      event.preventDefault()
+      statusElm.append('<div>please write a valid name</div>')
+    }
 
     if(email.length > 5 && email.includes('@') && email.includes('.')) {
       statusElm.append('')
@@ -31,6 +40,8 @@ $(document).ready(function () {
       event.preventDefault()
       statusElm.append('<div>please write a longer message</div>')
     }
+
+   
 
 
   })
