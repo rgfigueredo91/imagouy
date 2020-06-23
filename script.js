398d1859bb03580f5c621b6d59b0d8297ee4dbd5
Rodrigo Figueredo <59716658+rgfigueredo91@users.noreply.github.com>
2020-06-23 13:34:45 -0300

    de todo

---
+++ script.js
@@ -0,0 +1,60 @@
+
+const lightBoxContainer = document.querySelector(".lightbox");
+const lightboxImage = document.querySelector(".lightbox-img");
+const counter=document.querySelector(".lightbox-counter");
+const lightboxText=document.querySelector(".lightbox-text");
+const prevButton = document.querySelector(".prev")
+const nextButton = document.querySelector(".next")
+const portfolioItems =  document.querySelector(".hes-gallery").children;
+let index;
+let imgSrc;
+
+lightBoxContainer.addEventListener("click", function(event){
+    if(event.target!==lightboxImage && event.target!==prevButton && event.target!==nextButton){
+        ligthBox();
+    }
+})
+
+
+for (let i = 0; i<portfolioItems.length; i++){
+    portfolioItems[i].addEventListener("click", function() {
+        index=i;
+        changeImage();
+        ligthBox();
+    })
+}
+
+function next(){
+    if(index==portfolioItems.length -1){
+        index=0;
+    }
+    else {
+        index++;
+    }
+    changeImage();
+    
+ 
+}
+
+function prev(){
+    if(index==0){
+        index=portfolioItems.length-1;
+    }
+    else {
+        index--;
+    }
+    changeImage();
+}
+
+
+function ligthBox(){
+   lightBoxContainer.classList.toggle("open");
+}
+
+function changeImage(){
+    imgSrc=portfolioItems[index].querySelector("img").getAttribute("data-fullsize");
+    lightboxImage.src=imgSrc;
+    counter.innerHTML=(index + 1) + " of " + portfolioItems.length;
+    lightboxText.innerHTML=portfolioItems[index].querySelector("img").getAttribute("data-subtext");
+}
+
