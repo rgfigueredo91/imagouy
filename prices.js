be9a719e902a840cecb2bb5126b2815fe26b2630
Rodrigo Figueredo <59716658+rgfigueredo91@users.noreply.github.com>
2020-06-24 11:57:49 -0300

    sa

--- prices.js
+++ prices.js
@@ -1,5 +1,6 @@
 function calc()
             {
+				let model = document.getElementById('model').value;
 				let experienceType = document.getElementById('experience').value;
 				let numberInterior = document.getElementById('interior').value;
 				let numberExterior = document.getElementById('exterior').value;
@@ -10,19 +11,46 @@ function calc()
 				
 
                 
-                if(experienceType === 'light-experience' && porcentaje <= 0.25){
-                    document.getElementById('result').value = (priceInterior + priceExterior) - ((priceExterior + priceInterior)*porcentaje) + ' USD';
-				}
-				if(experienceType === 'light-experience' && porcentaje > 0.25) {
-					document.getElementById('result').value = (priceExterior + priceInterior) - ((priceExterior + priceInterior)*maxPorcentaje) + ' USD';
+                if(experienceType === 'light-experience' && porcentaje <= 0.25 && model === "no"){
+                    document.getElementById('result').value = (priceInterior + priceExterior) - ((priceExterior + priceInterior)*porcentaje) + 120 + ' USD';
 				}
 
-				if(experienceType === 'full-experience' && porcentaje <= 0.25) {
+				if(experienceType === 'light-experience' && porcentaje <= 0.25 && model === "yes"){
+                    document.getElementById('result').value = (priceInterior + priceExterior) - ((priceExterior + priceInterior)*porcentaje) + ' USD';
+				}
+
+				if(experienceType === 'light-experience' && porcentaje > 0.25 && model === "no") {
+					document.getElementById('result').value = (priceExterior + priceInterior) - ((priceExterior + priceInterior)*maxPorcentaje) + 120 + ' USD';
+				}
+
+				if(experienceType === 'light-experience' && porcentaje > 0.25 && model === "yes") {
+					document.getElementById('result').value = (priceExterior + priceInterior) - ((priceExterior + priceInterior)*maxPorcentaje)  + ' USD';
+				}
+
+				if(experienceType === 'full-experience' && porcentaje <= 0.25 && model === "no") {
+					var priceInterior = numberInterior * 220;
+					var priceExterior = numberExterior * 180;
+                    document.getElementById('result').value = (priceInterior +  priceExterior) - ((priceInterior +  priceExterior)*porcentaje) + 120 + ' USD';
+				}
+
+				if(experienceType === 'full-experience' && porcentaje <= 0.25 && model === "yes") {
 					var priceInterior = numberInterior * 220;
 					var priceExterior = numberExterior * 180;
                     document.getElementById('result').value = (priceInterior +  priceExterior) - ((priceInterior +  priceExterior)*porcentaje) + ' USD';
 				}
-				if(experienceType === 'full-experience' && porcentaje > 0.25) {
+
+
+
+
+
+
+				if(experienceType === 'full-experience' && porcentaje > 0.25 && model === "no") {
+					var priceInterior = numberInterior * 220;
+					var priceExterior = numberExterior * 180;
+					document.getElementById('result').value = (priceExterior + priceInterior) - ((priceExterior + priceInterior)*maxPorcentaje) + 120 + ' USD';
+				}
+
+				if(experienceType === 'full-experience' && porcentaje > 0.25 && model === "yes") {
 					var priceInterior = numberInterior * 220;
 					var priceExterior = numberExterior * 180;
 					document.getElementById('result').value = (priceExterior + priceInterior) - ((priceExterior + priceInterior)*maxPorcentaje) + ' USD';
