629416d8515e6bd6e2a55b07ca48fc046612e597
Rodrigo Figueredo <59716658+rgfigueredo91@users.noreply.github.com>
2020-06-25 02:12:37 -0300

    s

--- prices.js
+++ prices.js
@@ -1,117 +1,92 @@
+const MAX_RENDERING_COUNT_DISCOUNT_RATE = 0.25;
+
+const FULL_EXPERIENCE_INDOOR_RENDERING_UNIT_PRICE = 220;
+const FULL_EXPERIENCE_OUTDOOR_RENDERING_UNIT_PRICE = 180;
+
+const LIGHT_EXPERIENCE_INDOOR_RENDERING_UNIT_PRICE = 140;
+const LIGHT_EXPERIENCE_OUTDOOR_RENDERING_UNIT_PRICE = 120;
+
+function computeModelPriceByMeter(area) {
+	// area: int (in square meters)
+	//
+	// returns: int o float
+
+	if (0 < area && area <= 50) {
+		return area;
+	} else if (50 < area && area <= 100) {
+		return 50 + area * 0.5;
+	} else if (100 < area && area <= 1000) {
+		return 100 + area * 0.05;
+	} else {
+		return 160;
+	}
+}
+
+function computeModelPrice(unit, area) {
+	// unit: str ("m2" o "sqft")
+	// area: int
+	//
+	// returns: int o float
+
+	if (unit == "sqft") {
+		area *= 0.092903;
+	}
+
+	return computeModelPriceByMeter(area);
+}
+
+function computeRenderingCountDiscountedPrice(isFullExperience, indoorRenderingCount, outdoorRenderingCount) {
+	// isFullExperience: boolean
+	// indoorRenderingCount: int
+	// outdoorRenderingCount: int
+	//
+	// returns: int o float
+
+	const renderingCount = indoorRenderingCount + outdoorRenderingCount;
+	const renderingCountDiscountRate = Math.min(0.05 * (renderingCount - 1), MAX_RENDERING_COUNT_DISCOUNT_RATE);
+	const renderingCountRate = 1 - renderingCountDiscountRate;
+
+	const indoorRenderingUnitPrice = isFullExperience ? FULL_EXPERIENCE_INDOOR_RENDERING_UNIT_PRICE : LIGHT_EXPERIENCE_INDOOR_RENDERING_UNIT_PRICE;
+	const outdoorRenderingUnitPrice = isFullExperience ? FULL_EXPERIENCE_OUTDOOR_RENDERING_UNIT_PRICE : LIGHT_EXPERIENCE_OUTDOOR_RENDERING_UNIT_PRICE;
+
+	const indoorRenderingFullPrice = indoorRenderingCount * indoorRenderingUnitPrice;
+	const outdoorRenderingFullPrice = outdoorRenderingCount * outdoorRenderingUnitPrice;
+
+	return (indoorRenderingFullPrice + outdoorRenderingFullPrice) * renderingCountRate;
+}
+
+function computePrice(userAlreadyHasAModel, modelAreaUnit, modelArea, isFullExperience, indoorRenderingCount, outdoorRenderingCount) {
+	// userAlreadyHasAModel: boolean
+	// modelAreaUnit: str ("m2" o "sqft")
+	// modelArea: int
+	// isFullExperience: boolean
+	// indoorRenderingCount: int
+	// outdoorRenderingCount: int
+	//
+	// returns: int o float
+
+	const modelPrice = userAlreadyHasAModel ? 0 : computeModelPrice(modelAreaUnit, modelArea);
+	const renderingDiscountedPrice = computeRenderingCountDiscountedPrice(isFullExperience, indoorRenderingCount, outdoorRenderingCount);
+
+	return modelPrice + renderingDiscountedPrice;
+}
+
+////////////////////////////////////////////////////
+
 const selectElement = document.getElementById('model');
 
-selectElement.addEventListener('change', (event) => {
-	if (selectElement.value === "yes"){
-	document.getElementById('metros').disabled = true;
-	document.getElementById('units').disabled = true;
-	}
-	if (selectElement.value === "no"){
-		document.getElementById('metros').disabled = false;
-		document.getElementById('units').disabled = false;
+selectElement.addEventListener('change', event => {
+	document.getElementById('metros').disabled = selectElement.value === "yes";
+	document.getElementById('units').disabled = selectElement.value === "yes";
+});
+
+function computePriceFromForm() {
+	const userAlreadyHasAModel = document.getElementById('model').value === "yes";
+	const modelAreaUnit = document.getElementById('units').value;
+	const modelArea = Math.max(parseInt(document.getElementById('metros').value), 0);
+	const experienceType = document.getElementById('experience').value === "full-experience";
+	const indoorRenderingCount = Math.max(parseInt(document.getElementById('interior').value), 0);
+	const outdoorRenderingCount = Math.max(parseInt(document.getElementById('exterior').value), 0);
+
+	document.getElementById('result').value = Math.round(computePrice(userAlreadyHasAModel, modelAreaUnit, modelArea, experienceType, indoorRenderingCount, outdoorRenderingCount)) + ' USD';
 }
-  });
-
-function precioSegunMetro(){
-
-	var units = document.getElementById('units').value;
-	var priceMeter = parseFloat((document.getElementById('metros').value));
-
-	//precio por metro cuadrado
-
-	if (units === "m2" && priceMeter > 0 && priceMeter <= 50) {
-		priceMeter = priceMeter;
-	}
-
-	if (units === "m2" && priceMeter > 50 && priceMeter <= 100 ){
-		priceMeter = 50 + (priceMeter  * 0.5);
-	}
-
-	if (units === "m2" && priceMeter > 100 && priceMeter <= 1000 ){
-		priceMeter = 100 + (priceMeter * 0.05);
-	}
-
-	if (units === "m2" && priceMeter > 1000 ){
-		priceMeter = 160;
-	}
-
-	//precio por pie cuadrado
-	if (units === "sqft" && priceMeter > 0 && priceMeter <= 538) {
-		priceMeter =  priceMeter * 0.09;
-	}
-	if (units === "sqft" && priceMeter > 538 && priceMeter <= 1076) {
-		priceMeter = 50 + (priceMeter * 0.046);
-	}
-	if (units === "sqft" && priceMeter > 1076 && priceMeter <= 10764) {
-		priceMeter = 100 + (priceMeter * 0.0046);
-	}
-	if (units === "sqft" && priceMeter > 10764) {
-		priceMeter = 160;
-	}
-
-	return priceMeter;
-
-}
-
-function calc()
-            {
-				var model = document.getElementById('model').value;
-				let experienceType = document.getElementById('experience').value;
-				let numberInterior = document.getElementById('interior').value;
-				let numberExterior = document.getElementById('exterior').value;
-				let maxPorcentaje =  0.25;
-				var priceInterior = numberInterior * 140;
-				var priceExterior = numberExterior * 120;
-				let porcentaje = (((1*numberInterior) + (1*numberExterior) - 1)*5)/100;
-				
-				//no tiene modelado en experiencia light porcentajes menores de 25 y 25
-                if(experienceType === 'light-experience' && porcentaje <= 0.25 && model === "no"){
-                    document.getElementById('result').value = (priceInterior + priceExterior) - ((priceExterior + priceInterior)*porcentaje) + precioSegunMetro() + ' USD';
-				}
-
-				//tiene modelado en experiencia light porcentajes menores de 25 y 25
-				if(experienceType === 'light-experience' && porcentaje <= 0.25 && model === "yes"){
-					
-                    document.getElementById('result').value = (priceInterior + priceExterior) - ((priceExterior + priceInterior)*porcentaje) + ' USD';
-				}
-				//no tiene modelado en experiencia light siempre aplica porcentaje de 25
-				if(experienceType === 'light-experience' && porcentaje > 0.25 && model === "no") {
-					document.getElementById('result').value = (priceExterior + priceInterior) - ((priceExterior + priceInterior)*maxPorcentaje) + precioSegunMetro() + ' USD';
-				}
-				//tiene modelado en experiencia light siempre aplica porcentaje de 25
-				if(experienceType === 'light-experience' && porcentaje > 0.25 && model === "yes") {
-					document.getElementById('result').value = (priceExterior + priceInterior) - ((priceExterior + priceInterior)*maxPorcentaje)  + ' USD';
-				}
-
-
-
-				if(experienceType === 'full-experience' && porcentaje <= 0.25 && model === "no") {
-					var priceInterior = numberInterior * 220;
-					var priceExterior = numberExterior * 180;
-                    document.getElementById('result').value = (priceInterior +  priceExterior) - ((priceInterior +  priceExterior)*porcentaje) + precioSegunMetro() + ' USD';
-				}
-
-				if(experienceType === 'full-experience' && porcentaje <= 0.25 && model === "yes") {
-					var priceInterior = numberInterior * 220;
-					var priceExterior = numberExterior * 180;
-                    document.getElementById('result').value = (priceInterior +  priceExterior) - ((priceInterior +  priceExterior)*porcentaje) + ' USD';
-				}
-
-				if(experienceType === 'full-experience' && porcentaje > 0.25 && model === "no") {
-					var priceInterior = numberInterior * 220;
-					var priceExterior = numberExterior * 180;
-					document.getElementById('result').value = (priceExterior + priceInterior) - ((priceExterior + priceInterior)*maxPorcentaje) + precioSegunMetro() + ' USD';
-				}
-
-				if(experienceType === 'full-experience' && porcentaje > 0.25 && model === "yes") {
-					var priceInterior = numberInterior * 220;
-					var priceExterior = numberExterior * 180;
-					document.getElementById('result').value = (priceExterior + priceInterior) - ((priceExterior + priceInterior)*maxPorcentaje) + ' USD';
-				}
-
-
-
-
-				if(numberInterior < 0 || numberExterior < 0 || precioSegunMetro() <= 0 && model !== "yes"){
-				document.getElementById('result').value = 0;
-				}
-			}
---
+++ tests.js
@@ -0,0 +1,24 @@
+function _almostEqual(a, b) {
+    return Math.abs(a - b) < 0.1;
+}
+
+function testComputeModelPrice() {
+    return _almostEqual(computeModelPrice("m2", 300), 115);
+}
+
+function testComputeModelPriceSqFt() {
+    return _almostEqual(computeModelPrice("sqft", 2000), 109.2);
+}
+
+function testComputeRenderingCountDiscountedPrice() {
+    return _almostEqual(computeRenderingCountDiscountedPrice(true, 2, 2), 680);
+}
+
+function testComputePrice() {
+    return _almostEqual(computePrice(false, "m2", 300, true, 2, 2), 795);
+}
+
+test_functions = [testComputeModelPrice, testComputeModelPriceSqFt, testComputeRenderingCountDiscountedPrice, testComputePrice];
+for (let test_function of test_functions) {
+    console.log(test_function());
+}
