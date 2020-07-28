c7c6abd52f5d48c2a203c9f93d48eaad15f46ce1
Rodrigo Figueredo <59716658+rgfigueredo91@users.noreply.github.com>
2020-07-28 15:51:16 -0300

    s

---
+++ js/load.js
@@ -0,0 +1,10 @@
+const portfolio =  document.querySelectorAll(".img");
+
+let showImg =  setTimeout(function (){
+    portfolio.forEach(el=>el.style.opacity="1")}, 3000);
+
+    
+
+
+    
+    
---
+++ js/navbarmove.js
@@ -0,0 +1,12 @@
+var lastScrollTop = 100;
+			navbar = document.getElementById("navbar");
+			window.addEventListener("scroll", function () {
+				var scrollTop = window.pageYOffset || document
+					.documentElement.scrollTop;
+				if (scrollTop > lastScrollTop) {
+					navbar.style.top = "-100px";
+				} else {
+					navbar.style.top = "0";
+				}
+				lastScrollTop = scrollTop;
+			})
