460eeb8cc581b64f27fa2207582894d47e1ee0be
Rodrigo Figueredo <59716658+rgfigueredo91@users.noreply.github.com>
2020-08-31 12:28:48 -0300

    as

--- js/load.js
+++ js/load.js
@@ -3,7 +3,7 @@ const array = ["img/common/1x1Sf.jpg", "img/common/2x1Vf.jpg", "img/common/2x1Hl
 "img/common/2x1Vh.jpg", "img/common/1x1Sd.jpg", "img/common/1x1Bh.jpg", 
 "img/common/1x1Bg.jpg", "img/common/1x1Bd.jpg", "img/common/1x1Sh.jpg", 
 "img/common/2x1Hb.jpg", "img/common/1x1Bc.jpg", "img/common/2x1Va.jpg",
-"img/common/1x1Sa.jpg", "img/common/2x1Hc.jpg", "img/common/2x1Vi.jpg", "img/common/2x1Vg.jpg",
+"img/common/1x1Sj.jpg", "img/common/2x1Hc.jpg", "img/common/2x1Vi.jpg", "img/common/2x1Vg.jpg",
 "img/common/1x1Bf.jpg", "img/common/1x1Se.jpg", "img/common/1x1Bb.jpg",
 "img/common/2x1Hz.jpg"]
 for (i=0; i < image.length; i++) {
