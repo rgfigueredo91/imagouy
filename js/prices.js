8b5b61af50ada386eea45ebadc23f8f7093e287c
Rodrigo Figueredo <59716658+rgfigueredo91@users.noreply.github.com>
2021-03-23 18:21:21 -0300

    a

--- js/prices.js
+++ js/prices.js
@@ -1,10 +1,9 @@
-const MAX_RENDERING_COUNT_DISCOUNT_RATE = 0.20;
-
-let FULL_EXPERIENCE_INDOOR_RENDERING_UNIT_PRICE = 200;
-let FULL_EXPERIENCE_OUTDOOR_RENDERING_UNIT_PRICE = 180;
-
-let LIGHT_EXPERIENCE_INDOOR_RENDERING_UNIT_PRICE = 150;
-let LIGHT_EXPERIENCE_OUTDOOR_RENDERING_UNIT_PRICE = 120;
+let STUDENT_LIGHT_EXPERIENCE_INDOOR_RENDERING_UNIT_PRICE = 150;
+let STUDENT_LIGHT_EXPERIENCE_OUTDOOR_RENDERING_UNIT_PRICE = 160;
+let STUDENT_FULL_EXPERIENCE_INDOOR_RENDERING_UNIT_PRICE = 220;
+let STUDENT_FULL_EXPERIENCE_OUTDOOR_RENDERING_UNIT_PRICE = 230;
+let NORMAL_RENDER_INDOOR = 330;
+let NORMAL_RENDER_OUDDOOR = 350;
 
 const USER_COUNTRY = "UY";
 
@@ -16,105 +15,103 @@ fetch("https://ipinfo.io", {headers: {Accept: "application/json"}}).then(respons
 		
 	}
 	else {
-		LIGHT_EXPERIENCE_INDOOR_RENDERING_UNIT_PRICE = 1000;
-		LIGHT_EXPERIENCE_OUTDOOR_RENDERING_UNIT_PRICE = 1000;
-		FULL_EXPERIENCE_OUTDOOR_RENDERING_UNIT_PRICE = 1000;
-		FULL_EXPERIENCE_INDOOR_RENDERING_UNIT_PRICE = 1000;
+		STUDENT_LIGHT_EXPERIENCE_INDOOR_RENDERING_UNIT_PRICE = 230;
+		STUDENT_LIGHT_EXPERIENCE_OUTDOOR_RENDERING_UNIT_PRICE = 250;
+		STUDENT_FULL_EXPERIENCE_OUTDOOR_RENDERING_UNIT_PRICE = 350;
+		STUDENT_FULL_EXPERIENCE_INDOOR_RENDERING_UNIT_PRICE = 370;
+		NORMAL_RENDER_INDOOR = 450;
+		NORMAL_RENDER_OUDDOOR = 500;
 		console.log(json.country);
 	}
 });
 
-function computeModelPriceByMeter(area) {
-	// area: int (in square meters)
-	//
-	// returns: int o float
 
-	if (0 <= area && area <= 20) {
-		return area * 2.0;
-	} else if (20 < area && area <= 50) {
-		return 40 + (area-20) * 1;
-	} else if (50 < area && area <= 100) {
-		return 60 + (area-40) * 1;
-	} else if (100 < area && area <= 1000) {
-		return 115 + area * 0.05;
-	} else if (1000 < area && area <= 1500) {
-		return 100 + area * 0.07;
-	} else {
-		return 210;
+let clicked = false;
+
+function changeToggleDisabledEnabled(){
+	if(!clicked) {
+		clicked = true;
+		document.getElementById("experience").disabled=false;
+		console.log("Im student")
+	} else{
+		clicked = false;
+		document.getElementById("experience").disabled=true;
+		console.log("Im not student")
 	}
 }
 
-function computeModelPrice(unit, area) {
-	// unit: str ("m2" o "sqft")
-	// area: int
-	//
-	// returns: int o float
-
-	if (unit == "sqft") {
-		area *= 0.092903;
+function addingParagraphOnExperience() {
+	let experiences = document.getElementById("experience");
+	let htmlContentToAppend = "";
+	if (experiences.value == "light-experience"){
+		console.log("probando si funciona")
+		htmlContentToAppend = `
+		<p>This experience will give freedoom to the 3d artist to design the image.
+		Reducing contact to an initial meeting and a camera viewpoint correction prior to rendering.
+		After this, the final image is rendered, post-produced and sent. This does not affect the quality of the work, 
+		it just speeds up the work process. If you want to find out more about this workflow please contact us. 
+	</p>
+		`
 	}
+	else if (experiences.value == "full-experience"){
+		console.log("probando si funcionaB")
+		htmlContentToAppend = `
+		<p>This experience is based on a greater bond between the team and you, generating correction instances, in which it is possible to make changes 
+		(material textures, lights and equipment) on images already rendered, having more control of the image. 
+		If you want to find out more about this workflow please contact us.
+	</p>
+		`
+	}
+	document.getElementById("jsExperienceid").innerHTML = htmlContentToAppend;
+};
 
-	return computeModelPriceByMeter(area);
-}
+function addingImageInteriorScope() {
+	let slider = document.getElementById('interior-slider');
+	let htmlContentToAppend = "";
+	console.log(slider.value)
+	if (slider.value == "1"){
+		htmlContentToAppend = `
+		<img  src="img/imgscope/2x1Vh.jpg" data-src="" >
+		`
+	}
+	else if (slider.value == "2"){
+		htmlContentToAppend = `
+		<img  src="img/imgscope/1x1Bm.jpg" data-src="" >
+		`
+	}
+	else if (slider.value == "3"){
+		htmlContentToAppend = `
+		<img  src="img/imgscope/1x1Bp.jpg" data-src="" >
+		`
+	}
+	document.getElementById("images-scopes").innerHTML = htmlContentToAppend;
+};
 
-function computeRenderingCountDiscountedPrice(isFullExperience, indoorRenderingCount, outdoorRenderingCount) {
-	// isFullExperience: boolean
-	// indoorRenderingCount: int
-	// outdoorRenderingCount: int
-	//
-	// returns: int o float
-
-	const renderingCount = indoorRenderingCount + outdoorRenderingCount;
-	const renderingCountDiscountRate = Math.min(0.05 * (renderingCount - 1), MAX_RENDERING_COUNT_DISCOUNT_RATE);
-	const renderingCountRate = 1 - renderingCountDiscountRate;
-
-	const indoorRenderingUnitPrice = isFullExperience ? FULL_EXPERIENCE_INDOOR_RENDERING_UNIT_PRICE : LIGHT_EXPERIENCE_INDOOR_RENDERING_UNIT_PRICE;
-	const outdoorRenderingUnitPrice = isFullExperience ? FULL_EXPERIENCE_OUTDOOR_RENDERING_UNIT_PRICE : LIGHT_EXPERIENCE_OUTDOOR_RENDERING_UNIT_PRICE;
-
-	const indoorRenderingFullPrice = indoorRenderingCount * indoorRenderingUnitPrice;
-	const outdoorRenderingFullPrice = outdoorRenderingCount * outdoorRenderingUnitPrice;
-
-	return (indoorRenderingFullPrice + outdoorRenderingFullPrice) * renderingCountRate;
-}
-
-function computePrice(userAlreadyHasAModel, modelAreaUnit, modelArea, isFullExperience, indoorRenderingCount, outdoorRenderingCount) {
-	// userAlreadyHasAModel: boolean
-	// modelAreaUnit: str ("m2" o "sqft")
-	// modelArea: int
-	// isFullExperience: boolean
-	// indoorRenderingCount: int
-	// outdoorRenderingCount: int
-	//
-
-	// returns: int o float
-
-
-	const modelPrice = userAlreadyHasAModel ? 0 : computeModelPrice(modelAreaUnit, modelArea);
-	const renderingDiscountedPrice = computeRenderingCountDiscountedPrice(isFullExperience, indoorRenderingCount, outdoorRenderingCount);
-
-	return modelPrice + renderingDiscountedPrice;
-}
-
-////////////////////////////////////////////////////
-
-const selectElement = document.getElementById('model');
-
-selectElement.addEventListener('change', event => {
-	document.getElementById('metros').disabled = selectElement.value === "yes";
-	document.getElementById('units').disabled = selectElement.value === "yes";
-});
+function addingImageExteriorScope() {
+	let slider = document.getElementById('exterior-slider');
+	let htmlContentToAppend = "";
+	console.log(slider.value)
+	if (slider.value == "1"){
+		htmlContentToAppend = `
+		<img  src="img/imgscope/2x1Hh.jpg" data-src="" >
+		`
+	}
+	else if (slider.value == "2"){
+		htmlContentToAppend = `
+		<img  src="img/imgscope/1x1Bk.jpg" data-src="" >
+		`
+	}
+	else if (slider.value == "3"){
+	
+		htmlContentToAppend = `
+		<img  src="img/imgscope/2x1Hza.jpg" data-src="" >
+		`
+	}
+	document.getElementById("images-scopes-exterior").innerHTML = htmlContentToAppend;
+};
 
 
 
 
 
-function computePriceFromForm() {
-	const userAlreadyHasAModel = document.getElementById('model').value === "yes";
-	const modelAreaUnit = document.getElementById('units').value;
-	const modelArea = Math.max(parseInt(document.getElementById('metros').value), 0);
-	const experienceType = document.getElementById('experience').value === "full-experience";
-	const indoorRenderingCount = Math.max(parseInt(document.getElementById('interior').value), 0);
-	const outdoorRenderingCount = Math.max(parseInt(document.getElementById('exterior').value), 0);
 
-	document.getElementById('result').value = Math.ceil(computePrice(userAlreadyHasAModel, modelAreaUnit, modelArea, experienceType, indoorRenderingCount, outdoorRenderingCount) * 0.85) + ' $ - ' + Math.ceil(computePrice(userAlreadyHasAModel, modelAreaUnit, modelArea, experienceType, indoorRenderingCount, outdoorRenderingCount) * 1.23) + ' $';
-}
