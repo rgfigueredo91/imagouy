80c8de446f84e5bcd7e7fcdb4e35001116d31ecd
Rodrigo Figueredo <59716658+rgfigueredo91@users.noreply.github.com>
2021-04-05 16:38:07 -0300

    v

--- js/navbarmove.js
+++ js/navbarmove.js
@@ -1,12 +1,18 @@
-var lastScrollTop = 100;
+let lastScrollTop = 100;
 			navbar = document.getElementById("navbar-noboots");
+			navbarphone = document.getElementById("phonenavbarid");
 			window.addEventListener("scroll", function () {
 				var scrollTop = window.pageYOffset || document
 					.documentElement.scrollTop;
 				if (scrollTop > lastScrollTop) {
 					navbar.style.top = "-100px";
+					navbarphone.style.top = "-80px";
 				} else {
 					navbar.style.top = "0";
+					navbarphone.style.top = "0";
 				}
 				lastScrollTop = scrollTop;
 			})
+
+
+			
