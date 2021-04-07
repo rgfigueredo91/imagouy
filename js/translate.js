22f654c6ac6e66642931c4d75ef9e6799ffc98dd
Rodrigo Figueredo <59716658+rgfigueredo91@users.noreply.github.com>
2021-04-07 00:38:17 -0300

    g

--- js/translate.js
+++ js/translate.js
@@ -8,7 +8,9 @@ document.addEventListener("DOMContentLoaded", function(e){
             artists: " artistas",
             process: " procesos",
             pricess: " precios",
-            references: " referencias"
+            references: " referencias",
+            portfolio: " portafolio",
+            visualization: " 360 vis"
         }
     };
     //define language via window hash
@@ -22,6 +24,8 @@ document.addEventListener("DOMContentLoaded", function(e){
         document.getElementById("process-translate").textContent = language.esp.process;
         document.getElementById("prices-translate").textContent = language.esp.pricess;
         document.getElementById("references-translate").textContent = language.esp.references;
+        document.getElementById("portfolio-translate").textContent = language.esp.portfolio;
+        document.getElementById("360-translate").textContent = language.esp.visualization;
         
         
         //phone
@@ -30,15 +34,29 @@ document.addEventListener("DOMContentLoaded", function(e){
         document.getElementById("prices-translate-phone").textContent = language.esp.pricess;
         document.getElementById("references-translate-phone").textContent = language.esp.references;
         document.getElementById("contact-translate-phone").textContent = language.esp.contact;
+        document.getElementById("portfolio-translate-phone").textContent = language.esp.portfolio;
+        document.getElementById("360-translate-phone").textContent = language.esp.visualization;
 
-        //mantener el #eng cuando hago click en los links
 
-        document.getElementById("link-font").href = "index.html#esp";
+        //desktop mantener el #eng cuando hago click en los links 
+
+      
         document.getElementById("contactid").href = "contact.html#esp";
         document.getElementById("artist-translate").href = "artists.html#esp";
         document.getElementById("prices-translate").href = "pricing.html#esp";
         document.getElementById("references-translate").href = "reviews.html#esp";
+        document.getElementById("process-translate").href = "process.html#esp";
+        document.getElementById("portfolio-translate").href = "index.html#esp";
+        document.getElementById("360-translate").href = "360.html#esp";
 
+          //phone mantener el #eng cuando hago click en los links
+        document.getElementById("contact-translate-phone").href = "contact.html#esp";
+        document.getElementById("artist-translate-phone").href = "artists.html#esp";
+        document.getElementById("prices-translate-phone").href = "pricing.html#esp";
+        document.getElementById("references-translate-phone").href = "reviews.html#esp";
+        document.getElementById("process-translate-phone").href = "process.html#esp";
+        document.getElementById("portfolio-translate-phone").href = "index.html#esp";
+        document.getElementById("360-translate-phone").href = "360.html#esp";
         
     }   
  
