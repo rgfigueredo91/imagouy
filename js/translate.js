4b249fad4f31cac869e5992cd7dace523c6ed8ff
Rodrigo Figueredo <59716658+rgfigueredo91@users.noreply.github.com>
2021-04-06 22:14:17 -0300

    a

---
+++ js/translate.js
@@ -0,0 +1,45 @@
+
+document.addEventListener("DOMContentLoaded", function(e){
+    let language = {
+        esp: {
+            works: " trabajos",
+            about: " conócenos",
+            contact: " contáctanos",
+            artists: " artistas",
+            process: " procesos",
+            pricess: " precios",
+            references: " referencias"
+        }
+    };
+    //define language via window hash
+    if (window.location.hash === "#esp") {
+        
+        //desktop
+        document.getElementById("link-font").textContent = language.esp.works;
+        document.getElementById("link-font-about").textContent = language.esp.about;
+        document.getElementById("contactid").textContent = language.esp.contact;
+        document.getElementById("artist-translate").textContent = language.esp.artists;
+        document.getElementById("process-translate").textContent = language.esp.process;
+        document.getElementById("prices-translate").textContent = language.esp.pricess;
+        document.getElementById("references-translate").textContent = language.esp.references;
+        
+        
+        //phone
+        document.getElementById("contactenglishphone").textContent = language.eng.contact;
+       
+
+        //mantener el #eng cuando hago click en los links
+        document.getElementById("link-font").href = "index.html#esp";
+        document.getElementById("contactid").href = "contact.html#esp";
+        document.getElementById("artist-translate").href = "artists.html#esp";
+        document.getElementById("prices-translate").href = "pricing.html#esp";
+        document.getElementById("references-translate").href = "reviews.html#esp";
+
+        
+    }   
+ 
+});
+
+function timedRefresh() {
+    setTimeout("location.reload(true);", 600);
+    }
