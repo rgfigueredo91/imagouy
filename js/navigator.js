b3c605c6db655e34c1cb00a4fe01ad6165d26ff0
Rodrigo Figueredo <59716658+rgfigueredo91@users.noreply.github.com>
2021-04-14 20:59:22 -0300

    ss

---
+++ js/navigator.js
@@ -0,0 +1,8 @@
+fetch("https://ipinfo.io", {headers: {Accept: "application/json"}}).then(response => response.json()).then(json => {
+	console.log(json)
+
+if (json.country === "UY" && window.location.hash != "#eng"){		
+		console.log("Estoy en Uruguay");
+		window.location.hash === "#esp";
+	}
+});
