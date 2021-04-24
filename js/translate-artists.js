0b27c74aaa0c84a431328ce11099dbc1b26cd9bb
Rodrigo Figueredo <59716658+rgfigueredo91@users.noreply.github.com>
2021-04-24 01:26:56 -0300

    A

--- js/translate-artists.js
+++ js/translate-artists.js
@@ -5,8 +5,8 @@ document.addEventListener("DOMContentLoaded", function(e){
             title: " Vamos a conocernos",
             paragraph: " Muchos viajes y extensa experiencia recibida en todo el mundo, formaron nuestra visión de la arquitectura, dándonos una dósis de inspiración que nos puso en marcha. Fundamos Imago Rendering Studio con pasión para evocar emociones y crear mucho valor.",
             title2: " Nuestro viaje",
-            title3: " CO-FUNDADOR-ARTISTA 3D",
-            paragraph2: " Hola!, soy arquitecto y artista 3D egresado de la Facultad de Arquitectura, Diseño y Urbanismo, Montevideo-Uruguay. Mi forma de pasar el tiempo siempre fue estar conectado con el arte, la arquitectura, la pintura y la fotografía, en el cual luego se transformó en una pasión por crear imágenes y animaciones en la computadora. Soy especialista en el desarrollo de visualizacones arquitectónicas, Arquitectura 3D, Gráficos por computadora y cualquier tipo de visualización en el campo de la arquitectura el urbanismo y la ingeniería civil.",
+            title3: " FUNDADOR - ARQUITECTO - ARTISTA 3D",
+            paragraph2: " Hola!, soy arquitecto y artista 3D egresado de la Facultad de Arquitectura, Diseño y Urbanismo, Montevideo-Uruguay. Mi forma de pasar el tiempo siempre fue estar conectado con el arte, la arquitectura, la pintura, la tecnología y la fotografía. Transformándose luego en una pasión por crear imágenes y animaciones en la computadora. Tengo fervor por el diseño y la creación. Soy especialista en el desarrollo de visualizacones arquitectónicas, Arquitectura 3D, Diseño Gráfico, Imágenes, Websites, Brochures, y todo lo que probablemente necesites para incrementar tus ventas."  ,   
             phototittle: " Fotos de viaje",
 
         }
