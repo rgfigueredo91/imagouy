53d89be6c7d4b14ad50eb7efd9979a9f13438d33
Rodrigo Figueredo <59716658+rgfigueredo91@users.noreply.github.com>
2022-03-14 22:37:55 -0300

    q

---
+++ ps-vr5/layama.scene.js
@@ -0,0 +1,47 @@
+// Created with Motiva Layama v1.5 https://www.motivacg.com/layama
+
+function getLayamaCameras()
+{
+   var layamaCameras = new BABYLON.SmartArray(0);
+   layamaCameras.push({n: "render interior0000", a: "CoronaCamera001", p: new BABYLON.Vector3(1853.1, 159.798, 2609.42), l: new BABYLON.Vector3(1953.1, 159.798, 2609.42)});
+   layamaCameras.push({n: "render interior0001", a: "CoronaCamera002", p: new BABYLON.Vector3(1700.33, 159.798, 2598.13), l: new BABYLON.Vector3(1800.33, 159.798, 2598.13)});
+   layamaCameras.push({n: "render interior0002", a: "CoronaCamera003", p: new BABYLON.Vector3(1781.23, 159.798, 2511.28), l: new BABYLON.Vector3(1881.23, 159.798, 2511.28)});
+   layamaCameras.push({n: "render interior0003", a: "CoronaCamera004", p: new BABYLON.Vector3(2043.49, 159.798, 2511.28), l: new BABYLON.Vector3(2143.49, 159.798, 2511.28)});
+   layamaCameras.push({n: "render interior0004", a: "CoronaCamera005", p: new BABYLON.Vector3(2290.39, 159.798, 2500.81), l: new BABYLON.Vector3(2390.39, 159.798, 2500.81)});
+   layamaCameras.push({n: "render interior0005", a: "CoronaCamera006", p: new BABYLON.Vector3(2290.39, 159.798, 2366.9), l: new BABYLON.Vector3(2390.39, 159.798, 2366.9)});
+   layamaCameras.push({n: "render interior0006", a: "CoronaCamera007", p: new BABYLON.Vector3(1996.76, 159.798, 2366.9), l: new BABYLON.Vector3(2096.76, 159.798, 2366.9)});
+   layamaCameras.push({n: "render interior0007", a: "CoronaCamera008", p: new BABYLON.Vector3(2546.4, 159.798, 2573.17), l: new BABYLON.Vector3(2646.4, 159.798, 2573.17)});
+   layamaCameras.push({n: "render interior0008", a: "CoronaCamera009", p: new BABYLON.Vector3(1920.56, 159.798, 2474.04), l: new BABYLON.Vector3(2020.56, 159.798, 2474.04)});
+   layamaCameras.push({n: "render interior0009", a: "CoronaCamera010", p: new BABYLON.Vector3(1906.44, 159.798, 2301.59), l: new BABYLON.Vector3(2006.44, 159.798, 2301.59)});
+   layamaCameras.push({n: "render interior0010", a: "CoronaCamera011", p: new BABYLON.Vector3(1622.4, 279.904, 2348.41), l: new BABYLON.Vector3(1722.4, 279.904, 2348.41)});
+   layamaCameras.push({n: "render interior0011", a: "CoronaCamera012", p: new BABYLON.Vector3(1917.75, 445.068, 2364.23), l: new BABYLON.Vector3(2017.75, 445.068, 2364.23)});
+   layamaCameras.push({n: "render interior0012", a: "CoronaCamera013", p: new BABYLON.Vector3(1961.3, 445.068, 2475.2), l: new BABYLON.Vector3(2061.3, 445.068, 2475.2)});
+   layamaCameras.push({n: "render interior0013", a: "CoronaCamera014", p: new BABYLON.Vector3(1843.56, 445.068, 2501.98), l: new BABYLON.Vector3(1943.56, 445.068, 2501.98)});
+   layamaCameras.push({n: "render interior0014", a: "CoronaCamera015", p: new BABYLON.Vector3(1656.78, 445.068, 2501.98), l: new BABYLON.Vector3(1756.78, 445.068, 2501.98)});
+   layamaCameras.push({n: "render interior0015", a: "CoronaCamera016", p: new BABYLON.Vector3(1639.47, 445.068, 2673.95), l: new BABYLON.Vector3(1739.47, 445.068, 2673.95)});
+   layamaCameras.push({n: "render interior0016", a: "CoronaCamera017", p: new BABYLON.Vector3(2063.26, 445.068, 2669.88), l: new BABYLON.Vector3(2163.26, 445.068, 2669.88)});
+   layamaCameras.push({n: "render interior0017", a: "CoronaCamera018", p: new BABYLON.Vector3(2070.22, 445.068, 2508.23), l: new BABYLON.Vector3(2170.22, 445.068, 2508.23)});
+   layamaCameras.push({n: "render interior0018", a: "CoronaCamera019", p: new BABYLON.Vector3(2254.99, 445.068, 2476.71), l: new BABYLON.Vector3(2354.99, 445.068, 2476.71)});
+   layamaCameras.push({n: "render interior0019", a: "CoronaCamera020", p: new BABYLON.Vector3(2278, 445.068, 2660.96), l: new BABYLON.Vector3(2378, 445.068, 2660.96)});
+   layamaCameras.push({n: "render interior0020", a: "CoronaCamera021", p: new BABYLON.Vector3(2142.06, 445.068, 2356.53), l: new BABYLON.Vector3(2242.06, 445.068, 2356.53)});
+   return layamaCameras;
+}
+
+function getLayamaResolutions()
+{
+   var layamaResolutions = new BABYLON.SmartArray(0);
+   layamaResolutions.push("512");
+   layamaResolutions.push("512");
+   return layamaResolutions;
+}
+
+function getOnScreenLogoUsage()
+{
+   return 6;
+}
+
+function getLayamaControls()
+{
+   return {defMove: false, defRot: 1, altMove: true, altRot: 2};
+}
+
